89f435b932e4e8a2b9221e50e04f0e8facbd06b1
kosarzendehboodi <104859288+kzbdz@users.noreply.github.com>
2022-08-10 13:27:34 +0430

    css

--- assets/script/Equipment.js
+++ assets/script/Equipment.js
@@ -1,18 +1,21 @@
-let SuggestedEquipmentArray = [['me'],['uo'],['me '],['yo'],["67"],["56"],["e34"]]
-//var
 
-const clientchoice=document.querySelectorAll('#traveltype')
-const saveType=document.getElementById('save-type')
+//array for suggestion item
+let SuggestedEquipmentArray = [['me'], ['uo'], ['me '], ['yo'], ["67"], ["56"], ["e34"]]
 
 
+//varible
+const clientchoice = document.querySelectorAll('#traveltype')
+const saveType = document.getElementById('save-type')
 
 
-//event
 
-saveType.addEventListener('click',(e)=>{
-    e.preventDefault()
-    suggested(clientchoice)
-    
+
+//eventlistner
+
+saveType.addEventListener('click', (e) => {
+  e.preventDefault()
+  suggested(clientchoice)
+
 })
 
 
@@ -27,8 +30,8 @@ saveType.addEventListener('click',(e)=>{
 
 
 //function
- function suggested(typetavel){
- console.log(typetavel)
- 
-   
- }
+function suggested(typetavel) {
+  console.log(typetavel)
+
+
+}
